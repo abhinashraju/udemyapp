f9014d03c01294271e6c68b670e15bd14d401710
Abhinash Pakalapati <abhinashpakalapati@Abhinashs-MacBook-Pro.local>
2019-04-20 22:19:52 +0530

    Added db changes and new models for udemyappsandbox

--- config/db.php
+++ config/db.php
@@ -1,14 +1,15 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=udemyapp',
+        'username' => 'abhi',
+        'password' => 'abhi',
+        'charset' => 'utf8',
+        // Schema cache options (for production environment)
+        //'enableSchemaCache' => true,
+        //'schemaCacheDuration' => 60,
+        //'schemaCache' => 'cache',
+    ];
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
+    
